7650f53e5f23328be542bb9e89a01dac740eb53f
selangshe <guodongyu1192@163.com>
2018-10-29 16:38:10 +0800

    1.add:添加登陆界面

--- src/main/java/com/selangshe/message/sys/controller/textController.java
+++ src/main/java/com/selangshe/message/sys/controller/textController.java
@@ -1,17 +1,21 @@
 package com.selangshe.message.sys.controller;
 
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+
 
 @Controller
 public class textController {
 
     @RequestMapping(value = "/hello",method = RequestMethod.GET)
-    private String test(){
-        return  "/loginhh";
+    private String test(HttpServletRequest request){
+       // request.setAttribute("id","你好世界");
+
+        return  "/login";
     }
 
 }
